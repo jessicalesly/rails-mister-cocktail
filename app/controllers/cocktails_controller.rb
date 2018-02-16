665ecab4165f843a9f172ed839b991174f264ad0
Jessica Lesly <jk.lesly@gmail.com>
2018-02-16 14:49:00 +0100

    style improved

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -26,6 +26,12 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
   private
 
   def cocktail_params
